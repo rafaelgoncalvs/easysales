ac471881300069264b21a0df3bffe2423fbe6310
Rafael <rafaelgoncalvs@gmail.com>
2016-01-01 18:16:00 -0300

    Add new sale.

--- web/app.js
+++ web/app.js
@@ -2,25 +2,75 @@
 
 	var app = angular.module('easySales', []);
 
-	app.controller('SaleController', function() {		
+	app.controller('ListSaleController', function() {		
 		this.sales = salesTemp;
+	});
+
+	app.controller('SaleController', function($scope) {
+		this.sale = {};
+		this.addSale = function() {
+			salesTemp.unshift(this.sale);
+			this.sale = {};
+		};
 	});
 
 	var salesTemp = [
 		{
-			client: 'Rafael',
+			client: 'Rafael dos Santos Gonçalves',
 			date: '31/12/2015',
-			value: 259.56
+			value: 259.56,
+			products: [
+				{
+					name: 'Calça jeans azul',
+					value: 130.00
+				},
+				{
+					name: 'Blusa vermelha',
+					value: 70.00
+				},
+				{
+					name: 'Cinto Armani',
+					value: 9.56
+				}
+			]
 		}, 
 		{
-			client: 'Nathalia',
+			client: 'Nathalia Fernanda de Estefano Mazali Alves',
 			date: '10/11/2015',
-			value: 32.00
+			value: 32.10,
+			products: [
+				{
+					name: 'Meia fina preta',
+					value: 32.10
+				}
+			]
 		}, 
 		{
-			client: 'Maria',
+			client: 'Maria Aparecida de Souza',
 			date: '09/10/2015',
-			value: 1230.00
+			value: 1050.00,
+			products: [
+				{
+					name: 'Calça jeans azul com barra',
+					value: 200.00
+				},
+				{
+					name: 'Calça social preta',
+					value: 500.00
+				},
+				{
+					name: 'Blusa vermelha',
+					value: 100.00
+				},
+				{
+					name: 'Blusa azul',
+					value: 100.00
+				},
+				{
+					name: 'Blusa amarela',
+					value: 150.00
+				}
+			]
 		}
 	];
 

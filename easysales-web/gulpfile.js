ec81923c6a4fed216b009b02445c15dcbaf5ce8d
Rafael <rafaelgoncalvs@gmail.com>
2016-01-28 21:38:04 -0300

    Add gulp-watch in the web project.

--- easysales-web/gulpfile.js
+++ easysales-web/gulpfile.js
@@ -3,19 +3,27 @@ var browserify = require('browserify');
 var uglify = require('gulp-uglify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var watch = require('gulp-watch');
 
-gulp.task('scripts', function() {
+gulp.task('minifier', function() {
 	return gulp.src(['app/**/*.js'])
 			.pipe(uglify())
 			.pipe(gulp.dest('./'));
 });
 
-gulp.task('browserify', function() {
+gulp.task('bundle', function() {
 	return browserify({ entries: ['./app/main.js'] })
 	    .bundle()        
 	    .pipe(source('main.js'))
 	    .pipe(buffer())
 	    //.pipe(uglify())
 	    .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function() {
+	var watcher = gulp.watch('app/**/*.js', ['bundle']);
+	watcher.on('change', function(event) {
+  		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+	});
 });
 
